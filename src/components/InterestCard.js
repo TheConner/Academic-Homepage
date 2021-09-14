7b71d88e0349f914260c533cfa5c7496033877e6
Conner Bradley <bradley@advtech.ca>
2021-09-13 22:38:53 -0400

    Clean up technologies section

--- src/components/InterestCard.js
+++ src/components/InterestCard.js
@@ -6,7 +6,7 @@ const InterestCard = ({interestName, interestDesc}) => {
     const [isFlipped, setIsFlipped] = React.useState(false);
 
     return (
-        <div>
+        <>
             <Card 
                 className={isFlipped ? "card-container-visible" : ""}
                 onMouseDown={() => setIsFlipped((prev) => !prev)}>
@@ -15,7 +15,7 @@ const InterestCard = ({interestName, interestDesc}) => {
                     <div className={isFlipped? "card-transition card-content-visible" : "card-transition card-content-invisible"} dangerouslySetInnerHTML={{__html: interestDesc}}></div>
                 </Card.Content>
             </Card>
-        </div>
+        </>
       );
 
 }
